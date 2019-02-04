b358f8370383f1590096d50ebcd607de1a216d32
Krzysztof Stanaszek <kstanaszek@objectivity.co.uk>
2019-02-04 08:07:50 +0100

    slides final version

--- src/main/scala/functions/FunctionsAsValues.scala
+++ src/main/scala/functions/FunctionsAsValues.scala
@@ -3,7 +3,7 @@ package functions
 object FunctionsAsValues extends App {
   val add = (a: Int, b: Int) =>  a + b
 
-  def substract = (a: Int, b: Int) => a -b
+  def subtract = (a: Int, b: Int) => a - b
 
   println(add(1,2))
 }
